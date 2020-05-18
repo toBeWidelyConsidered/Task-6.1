da2d24a5ece21633e721ff1de1339f88bf1d3ede
ZoyaKhitrova <hitrova.zoya1533@gmail.com>
2020-05-18 22:15:38 +0300

    project

---
+++ Задача6.1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Задача6._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "А";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(345, 131);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "В";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 699);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Кнопки";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Задача6.1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "A";
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button2.Text = "B";
+            button2.BackColor = SystemColors.ControlDarkDark;
+            button2.Width = button1.Width;
+            button2.Height = button1.Height;
+            this.BackColor = SystemColors.WindowText;
+            this.Height = button1.Height*6;
+            this.Width = button1.Width*2;
+            this.MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Math.Abs(button1.Location.Y - button2.Location.Y) - 10 >= (button2.Height/2 + button1.Height/2))
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y + 10);
+                button2.Location = new Point(button2.Location.X, button2.Location.Y - 10);
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if ((button1.Location.Y - 10 >= 0) & (button2.Location.Y < 231))
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y - 10);
+                button2.Location = new Point(button2.Location.X, button2.Location.Y + 10);
+            }
+        }
+    }
+}
